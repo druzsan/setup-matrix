86028e19b99e50e6637e61956a2d28b0e7b2343b
Alexander Druz <alexandersdruz@gmail.com>
2024-02-06 21:29:32 +0100

    Debug Dockerfile

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import os
 import yaml
 
 
-def debug(text: str) -> None:
-    print(f"::debug::{text}")
-
-
 def output(name: str, value: str) -> None:
     with open(os.environ["GITHUB_OUTPUT"], "a", encoding="UTF-8") as output_file:
         output_file.write(f"{name}={value}\r\n")
@@ -24,7 +20,6 @@ if __name__ == "__main__":
     exclude = yaml.safe_load(os.environ["INPUT_EXCLUDE"])
 
     print("foo")
-    debug("bar")
     output("matrix", "{}")
     setenv("MATRIX", "{}")
 
